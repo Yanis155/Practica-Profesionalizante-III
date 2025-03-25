f849559e52c67306ebe0f2d3837b4889c3c1a9ba
Yanis155 <yanis.sco@gmail.com>
2025-03-25 20:15:24 -0300

    subo ejercicios 25/3

---
+++ Juego de batalla naval/Barco.js
@@ -0,0 +1,14 @@
+class Barco(){
+    
+    constructor(tipos, tamaño){
+        this.tipos= filas
+        this.tamanio= tamanio
+    }
+
+};
+
+
+
+
+
+
---
+++ Juego de batalla naval/Tablero.js
@@ -0,0 +1,15 @@
+class Tablero(){
+
+    var Tablero= [];
+    var i, j= 1;
+    crearTablero(){ //creamos el tablero osea lo dibujamos
+        
+
+    }
+    function asignarBarco(){ //asignamos los barcos al tablero
+
+    }
+
+    function 
+
+}
